fde9723a1ac0f5b72816422fdb8bb6d3a429d482
Angelina <angelina.ratnykova@gmail.com>
2018-07-08 15:57:44 +0300

    change

--- animation/index.js
+++ animation/index.js
@@ -1,29 +1,42 @@
 let input = document.querySelector('.search__field');
 let imagesGroup = document.querySelectorAll('.images__group');
+let image = document.querySelectorAll('.image');
 
 const keyDownHandler = (e) => {
     let searchQuery = e.target.value.toLowerCase()
 
     imagesGroup.forEach((item) => {
         if (searchQuery.indexOf('good ' + item.dataset.group) == 0 && searchQuery != '') {
-            item.children[0].classList.add('fadeInFirst')
-            item.children[1].classList.add('fadeInMid')
-            item.children[2].classList.add('fadeInMid')
-            item.children[3].classList.add('fadeInLast')
+            item.classList.remove('fadeOut')
+            setTimeout(() => {
+                item.children[0].classList.add('fadeInFirst')
+                item.children[1].classList.add('fadeInMid')
+                item.children[2].classList.add('fadeInMid')
+                item.children[3].classList.add('fadeInLast')
+            }, 300)
+            responsiveImages();
         }
         else {
-            item.children[1].classList.remove('fadeInMid')
-            item.children[0].classList.remove('fadeInFirst')
-            item.children[2].classList.remove('fadeInMid')
-            item.children[3].classList.remove('fadeInLast')
+            item.classList.add('fadeOut')
+            setTimeout(() => {
+                item.children[1].classList.remove('fadeInMid')
+                item.children[0].classList.remove('fadeInFirst')
+                item.children[2].classList.remove('fadeInMid')
+                item.children[3].classList.remove('fadeInLast')
+            }, 500)
+
         }
     })
 
 }
 
-const fadeIn = () => {
-
+const responsiveImages = () => {
+    image.forEach((img)=>{
+       // img.width = '50px'
+        console.log(img.clientWidth)
+    })
 }
+
 
 input.addEventListener('keyup', keyDownHandler, false);
 
