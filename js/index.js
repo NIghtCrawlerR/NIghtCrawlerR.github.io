7b901745cc300e24d39a3560e8f4e52f124506c4
Angelina <angelina.ratnykova@gmail.com>
2019-05-05 23:11:39 +0300

    add project to portfolio

--- js/index.js
+++ js/index.js
@@ -52,32 +52,15 @@ var projects = {
         descr: 'JavaScript timer with loops and share functions.',
         img: 'img/timer.png',
         link: 'https://github.com/NIghtCrawlerR/js-timer'
+    },
+    'moviedb': {
+        title: 'Movie Database',
+        descr: 'My wishlist of movies with filter and search functions. Uses: React, Node.js, MongoDB',
+        img: 'img/moviedb.jpg',
+        link: 'https://github.com/NIghtCrawlerR/MovieDB-React-Mongo-Node-'
     }
 }
 
-
-//parallax
-// var strength1 = 50;
-// var strength2 = 100;
-// var strength3 = 200;
-// $("html").mousemove(function (e) {
-//     var pageX = e.pageX - ($(window).width() / 2);
-//     var pageY = e.pageY - ($(window).height() / 2);
-//     var newvalueX = 1 * pageX * -1;
-//     var newvalueY = 1 * pageY * -1;
-//     if($(window).width() > 768){
-//         $('#background').css("background-position", (strength1 / $(window).width() * pageX * -1) + "px " + (strength1 / $(window).height() * pageY * -1) + "px");
-//     }
-    
-
-// });
-//end parallax
-
-//menu
-// $('.Menu__button').click(function () {
-//     $('.Side-menu').toggleClass('Side-menu--active');
-//     $(this).toggleClass('Menu__button--active');
-// })
 
 $('.wrapper-menu').click(function () {
     $('.Side-menu').toggleClass('Side-menu--active');
@@ -94,7 +77,7 @@ $('.Side-menu li, .navigation_hidden li').click(function () {
     setTimeout(function () {
         $('.sidepage-' + index).css('left', '0');
         $('.sidepage-' + index).addClass('sidepage--active');
-        if($(window).width() < 768){
+        if ($(window).width() < 768) {
             $('.sidepage-' + index).show();
             $('.Menu__button__mob').show()
         }
@@ -102,17 +85,17 @@ $('.Side-menu li, .navigation_hidden li').click(function () {
 
 })
 
-$('.Menu__button__mob').click(function(){
+$('.Menu__button__mob').click(function () {
     $('.sidepage').hide();
     $('.Menu__button__mob').hide();
 })
 //end menu
 
 //rocket animation
-$('.rocket-animated').click(function(){
-    $(this).removeClass('rocket-animated') 
-    $(this).addClass('rocket-animated--active') 
-    setTimeout(function() {
+$('.rocket-animated').click(function () {
+    $(this).removeClass('rocket-animated')
+    $(this).addClass('rocket-animated--active')
+    setTimeout(function () {
         $('.navigation_hidden').fadeIn(500);
     }, 2000)
 })
@@ -127,7 +110,7 @@ $(document).mouseup(function (e) {
         $(".sidepage").css('left', '-50%');
         $('.Side-menu li').removeClass('menu-item--active');
         $('body').removeClass('blur');
-       // $('.Side-menu').hide();
+        // $('.Side-menu').hide();
     }
 });
 
@@ -145,103 +128,151 @@ function selectProject(name) {
     $('.Project__image').css('background-image', 'url(' + projects[name].img + ')')
     $('.link_to_project').attr('href', projects[name].link)
 }
-
-//Amcharts
-var chart = AmCharts.makeChart("chartdiv", {
-    "type": "pie",
-    "startDuration": 0,
-    "theme": "light",
-    "addClassNames": true,
-    "innerRadius": "30%",
-    "color": "#fff",
-    "fontSize": 14,
-    //"colors": ["#333, #fff, #f1f1f1, #f2f2f2, #f3f3f3, #f4f4f4, #f5f5f5, #f6f6f6, #f7f7f7"],
-    "pieBaseColor": "#A65600", //094b73 - blue
-    "pieBrightnessStep": 15,
-    "outlineColor": "#A65600",
-    "outlineThickness": 1,
-    "radius": "30%",
-    "innerRadius": "50%",
-    "defs": {
-        "filter": [{
-            "id": "shadow",
-            "width": "200%",
-            "height": "200%",
-            "feOffset": {
-                "result": "offOut",
-                "in": "SourceAlpha",
-                "dx": 0,
-                "dy": 0
-            },
-            "feGaussianBlur": {
-                "result": "blurOut",
-                "in": "offOut",
-                "stdDeviation": 5
-            },
-            "feBlend": {
-                "in": "SourceGraphic",
-                "in2": "blurOut",
-                "mode": "normal"
-            }
+var ctx = document.getElementById('myChart').getContext('2d');
+ctx.height = 500;
+var myChart = new Chart(ctx, {
+    type: 'horizontalBar',
+    data: {
+        labels: ['HTML', 'CSS/LESS', 'Bootstrap', 'JavaScript (jQuery)', 'React', 'Gulp', 'Git', 'Node.js', 'MongoDB'],
+        datasets: [{
+            data: [10, 10, 8, 7, 5, 4, 5, 4, 4],
+            backgroundColor: 'rgba(255, 148, 0, .6)',
+            //backgroundColor: 'rgba(34, 170, 253, .5)',
+            borderColor: '#FF9400',
+            //borderColor: '#22aafd',
+            hoverBackgroundColor: '#FF9400',
+            borderWidth: 1
         }]
     },
-    "dataProvider": [{
-        "skill": "JavaScript",
-        "level": 95,
-        "color": "#520e1b"
-    }, {
-        "skill": "HTML",
-        "level": 150,
-        "color": "#5e0a1a"
-    }, {
-        "skill": "CSS",
-        "level": 150,
-        "color": "#c03435"
-    }, {
-        "skill": "ReactJS",
-        "level": 65,
-        "color": "#250917"
-    }, {
-        "skill": "Gulp",
-        "level": 50,
-        "color": "#ea2b3d"
-    }, {
-        "skill": "SCSS/LESS",
-        "level": 100,
-        "color": "#333"
-    }, {
-        "skill": "Photoshop",
-        "level": 75,
-        "color": "#333"
-    }, {
-        "skill": "Git",
-        "level": 85,
-        "color": "#333"
-    }, {
-        "skill": "Node.js",
-        "level": 70,
-        "color": "#333"
-    },{
-        "skill": "MongoDB",
-        "level": 65,
-        "color": "#333"
-    }],
-    // "colorField": "color",
-    "valueField": "level",
-    "titleField": "skill"
+    responsive: true,
+    maintainAspectRatio: false,
+    options: {
+        
+        legend: {
+            display: false
+        },
+
+        scales: {
+            yAxes: [{
+                barThickness: 15,
+                ticks: {
+                    beginAtZero: true,
+                    fontColor: '#fff',
+                    fontSize: 16
+                },
+                gridLines: {
+                    color: 'rgba(255, 255, 255, .05)'
+                },
+            }],
+            xAxes: [{
+                barThickness: 25,
+                gridLines: {
+                    color: 'rgba(255, 255, 255, .05)'
+                },
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
 });
+myChart.aspectRatio = 0;
+//Amcharts
+// var chart = AmCharts.makeChart("chartdiv", {
+//     "type": "pie",
+//     "startDuration": 0,
+//     "theme": "light",
+//     "addClassNames": true,
+//     "innerRadius": "30%",
+//     "color": "#fff",
+//     "fontSize": 14,
+//     //"colors": ["#333, #fff, #f1f1f1, #f2f2f2, #f3f3f3, #f4f4f4, #f5f5f5, #f6f6f6, #f7f7f7"],
+//     "pieBaseColor": "#A65600", //094b73 - blue
+//     "pieBrightnessStep": 15,
+//     "outlineColor": "#A65600",
+//     "outlineThickness": 1,
+//     "radius": "30%",
+//     "innerRadius": "50%",
+//     "defs": {
+//         "filter": [{
+//             "id": "shadow",
+//             "width": "200%",
+//             "height": "200%",
+//             "feOffset": {
+//                 "result": "offOut",
+//                 "in": "SourceAlpha",
+//                 "dx": 0,
+//                 "dy": 0
+//             },
+//             "feGaussianBlur": {
+//                 "result": "blurOut",
+//                 "in": "offOut",
+//                 "stdDeviation": 5
+//             },
+//             "feBlend": {
+//                 "in": "SourceGraphic",
+//                 "in2": "blurOut",
+//                 "mode": "normal"
+//             }
+//         }]
+//     },
+//     "dataProvider": [{
+//         "skill": "JavaScript",
+//         "level": 95,
+//         "color": "#520e1b"
+//     }, {
+//         "skill": "HTML",
+//         "level": 150,
+//         "color": "#5e0a1a"
+//     }, {
+//         "skill": "CSS",
+//         "level": 150,
+//         "color": "#c03435"
+//     }, {
+//         "skill": "ReactJS",
+//         "level": 65,
+//         "color": "#250917"
+//     }, {
+//         "skill": "Gulp",
+//         "level": 50,
+//         "color": "#ea2b3d"
+//     }, {
+//         "skill": "SCSS/LESS",
+//         "level": 100,
+//         "color": "#333"
+//     }, {
+//         "skill": "Photoshop",
+//         "level": 75,
+//         "color": "#333"
+//     }, {
+//         "skill": "Git",
+//         "level": 85,
+//         "color": "#333"
+//     }, {
+//         "skill": "Node.js",
+//         "level": 70,
+//         "color": "#333"
+//     },{
+//         "skill": "MongoDB",
+//         "level": 65,
+//         "color": "#333"
+//     }],
+//     // "colorField": "color",
+//     "valueField": "level",
+//     "titleField": "skill"
+// });
 
-chart.addListener("init", handleInit);
+// chart.addListener("init", handleInit);
 
-chart.addListener("rollOverSlice", function (e) {
-    handleRollOver(e);
-});
+// chart.addListener("rollOverSlice", function (e) {
+//     handleRollOver(e);
+// });
 
-function handleInit() {
-    chart.legend.addListener("rollOverItem", handleRollOver);
-}
+// function handleInit() {
+//     chart.legend.addListener("rollOverItem", handleRollOver);
+// }
 
-function handleRollOver(e) {
-    var wedge = e.dataItem.wedge.node;
-    wedge.parentNode.appendChild(wedge);
-}
+// function handleRollOver(e) {
+//     var wedge = e.dataItem.wedge.node;
+//     wedge.parentNode.appendChild(wedge);
+// }
